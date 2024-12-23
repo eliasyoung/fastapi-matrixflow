04eb017e3027f0c8f2a690a8c3cc64c6ca5fb075
YOUNG <12171147+eliasyoung@users.noreply.github.com>
2024-12-23 17:50:12 +0800

    refactor(matrixflow): update `updated_at` field to use client-side default
    - Remove server-side `server_onupdate` parameter
    - Use `onupdate=datetime.now()` for client-side default value

--- app/models/matrix_flow.py
+++ app/models/matrix_flow.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from sqlalchemy import Text, text
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
-import uuid
 
 from . import Base
 
@@ -14,5 +13,4 @@ class Matrixflow(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, index=True, server_default=text('gen_random_uuid()'))
     graph: Mapped[str] = mapped_column(Text)
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
-    updated_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'), server_onupdate=text('CURRENT_TIMESTAMP'))
-
+    updated_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'), onupdate=datetime.now())
